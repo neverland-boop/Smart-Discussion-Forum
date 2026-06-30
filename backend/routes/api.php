8955008979821e64f18e5bdd96077b89ad8ae440
neverland <neverland652@gmail.com>
2026-06-30 12:31:13 +0300

    Reqistered routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\TopicController;
+use App\Http\Controllers\Api\GroupController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,6 +14,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/onboard', [AuthController::class, 'onboard']);
     Route::get('/profile', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('groups', GroupController::class);
+    Route::apiResource('topics', TopicController::class);
+    Route::apiResource('posts', PostController::class);
+
     
     // Get the current user
     Route::get('/user', function (Request $request) {
