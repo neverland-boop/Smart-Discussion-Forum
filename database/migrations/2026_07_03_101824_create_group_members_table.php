fc1d00c3bfa890209875d563a9cec09177bfb9eb
neverland <neverland652@gmail.com>
2026-07-04 06:28:02 +0300

    Filled group memebr migration

--- database/migrations/2026_07_03_101824_create_group_members_table.php
+++ database/migrations/2026_07_03_101824_create_group_members_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('group_members', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+    Schema::create('group_members', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('group_id')->constrained()->onDelete('cascade');
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->timestamps();
+    });
     }
 
     /**
