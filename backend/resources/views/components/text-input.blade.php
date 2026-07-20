c2bd647d029e2cf576005f9fbc9a3570076bbb3d
Nuwagaba Duncan <neverland652@gmail.com>
2026-07-20 20:17:22 +0300

    Delete backend directory

--- backend/resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 bg-white dark:bg-[#121212] text-gray-900 dark:text-gray-300 focus:border-[#5CC98B] dark:focus:border-[#5CC98B] focus:ring-[#5CC98B] dark:focus:ring-[#5CC98B] rounded-md shadow-sm transition-colors duration-200']) !!}>
